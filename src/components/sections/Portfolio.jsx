5f41a8ef4264c7064239b2b981e0c33742360c59
jasleorez <pernardtie1901@gmail.com>
2025-02-12 04:30:12 +0800

    Change assets paths

--- src/components/sections/Portfolio.jsx
+++ src/components/sections/Portfolio.jsx
@@ -6,37 +6,37 @@ export const Portfolio = () => {
     {
       title: "HAU School Department Accreditation Status",
       category: ["graphic-design"],
-      image: "/assets/hauAccreditationStatus.jpeg",
+      image: "./assets/hauAccreditationStatus.jpeg",
       tech: ["Canva"],
     },
     {
       title: "Creo Sid Logo Design",
       category: ["logo-design", "graphic-design"],
-      image: "/assets/creoSidLogo.JPG",
+      image: "./assets/creoSidLogo.JPG",
       tech: ["Canva"],
     },
     {
       title: "Hotel Booking App",
       category: ["web-development"],
-      image: "/assets/hotelBookingApp.png",
+      image: "./assets/hotelBookingApp.png",
       tech: ["ReactJS", "EthersJS", "Hardhat"],
     },
     {
       title: "Simple Projex",
       category: ["web-design", "web-development"],
-      image: "/assets/simpleProjex.png",
+      image: "./assets/simpleProjex.png",
       tech: ["React", "NextJS", "Canva", "TailwindCSS"],
     },
     {
       title: "Coffee Shop Website",
       category: ["web-design"],
-      image: "/assets/coffeeShopWebsite.png",
+      image: "./assets/coffeeShopWebsite.png",
       tech: ["HTML", "CSS", "JavaScript"],
     },
     {
       title: "Simple Projex Logo",
       category: ["logo-design", "graphic-design"],
-      image: "/assets/simpleProjexLogo.png",
+      image: "./assets/simpleProjexLogo.png",
       tech: ["Canva"],
     },
   ];
